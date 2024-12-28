6a7bd6d6e07441a7878df3217610ccb9e865d983
Murat Ates <mrtt.atss@gmail.com>
2024-12-28 15:03:16 +0300

    request added

---
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/model/dto/request/ProductCreateRequest.java
@@ -0,0 +1,41 @@
+package com.muates.inventorymanagementsystem.model.dto.request;
+
+public class ProductCreateRequest {
+
+    private String name;
+    private Integer stockQuantity;
+    private Double price;
+    private Double discount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(Integer stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+}
---
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/model/dto/request/RetailerCreateRequest.java
@@ -0,0 +1,50 @@
+package com.muates.inventorymanagementsystem.model.dto.request;
+
+public class RetailerCreateRequest {
+
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+    private String password;
+    private String photoUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}
