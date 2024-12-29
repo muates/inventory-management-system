26b074d913bf70f957203263aab8750aeb39259b
Murat Ates <mrtt.atss@gmail.com>
2024-12-29 22:26:55 +0300

    auth context added

---
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/service/auth/context/AuthContext.java
@@ -0,0 +1,30 @@
+package com.muates.inventorymanagementsystem.service.auth.context;
+
+import com.muates.inventorymanagementsystem.service.auth.strategy.AuthStrategy;
+
+public class AuthContext {
+
+    private static AuthContext instance;
+    private AuthStrategy authStrategy;
+
+    private AuthContext(AuthStrategy authStrategy) {
+        this.authStrategy = authStrategy;
+    }
+
+    public static AuthContext getInstance(AuthStrategy authStrategy) {
+        if (instance == null) {
+            instance = new AuthContext(authStrategy);
+        } else {
+            instance.authStrategy = authStrategy;
+        }
+        return instance;
+    }
+
+    public boolean authenticate(String email, String password) {
+        return authStrategy.authenticate(email, password);
+    }
+
+    public void register(Object request) {
+        authStrategy.register(request);
+    }
+}
