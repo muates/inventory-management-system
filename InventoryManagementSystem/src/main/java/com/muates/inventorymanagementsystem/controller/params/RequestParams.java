fc3e96d22329c2dcef617937ef60f19cefefb835
Murat Ates <mrtt.atss@gmail.com>
2024-12-30 18:57:34 +0300

    product update request param added

--- InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/controller/params/RequestParams.java
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/controller/params/RequestParams.java
@@ -4,5 +4,6 @@ public class RequestParams {
     public static final String[] RETAILER_CREATE_REQUEST_PARAMS = {"name", "phoneNumber", "emailAddress", "password", "photoUrl"};
     public static final String[] SUPPLIER_CREATE_REQUEST_PARAMS = {"name", "phoneNumber", "emailAddress", "password", "photoUrl"};
     public static final String[] PRODUCT_CREATE_REQUEST_PARAMS = {"name", "stockQuantity", "price", "discount"};
+    public static final String[] PRODUCT_UPDATE_REQUEST_PARAMS = {"name", "stockQuantity", "price", "discount"};
     public static final String[] AUTH_LOGIN_REQUEST = {"emailAddress", "password"};
 }
