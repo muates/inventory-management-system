b3f9acf8d5171dcfe03753942a8933b518ec478c
Murat Ates <mrtt.atss@gmail.com>
2025-01-02 00:14:36 +0300

    refactoring

--- InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/common/mapper/RequestMapper.java
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/common/mapper/RequestMapper.java
@@ -2,6 +2,8 @@ package com.muates.inventorymanagementsystem.common.mapper;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
 
 public class RequestMapper {
 
@@ -14,8 +16,23 @@ public class RequestMapper {
 
                 String methodName = "set" + paramName.substring(0, 1).toUpperCase() + paramName.substring(1);
 
-                Method method = dtoClass.getMethod(methodName, String.class);
-                method.invoke(dto, value);
+                Field field = dtoClass.getDeclaredField(paramName);
+                Class<?> fieldType = field.getType();
+
+                Method method;
+                if (fieldType.equals(String.class)) {
+                    method = dtoClass.getMethod(methodName, String.class);
+                    method.invoke(dto, value);
+                } else if (fieldType.equals(Integer.class) || fieldType.equals(int.class)) {
+                    method = dtoClass.getMethod(methodName, Integer.class);
+                    method.invoke(dto, Integer.valueOf(value));
+                } else if (fieldType.equals(Double.class) || fieldType.equals(double.class)) {
+                    method = dtoClass.getMethod(methodName, Double.class);
+                    method.invoke(dto, Double.valueOf(value));
+                } else if (fieldType.equals(BigDecimal.class)) {
+                    method = dtoClass.getMethod(methodName, BigDecimal.class);
+                    method.invoke(dto, new BigDecimal(value));
+                }
             }
 
             return dto;
