f592bf051ec58b92c9cb1be0a7e9c727cb2668ba
Murat Ates <mrtt.atss@gmail.com>
2025-01-02 00:13:18 +0300

    findAllBySupplierId added

--- InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/repository/ProductRepository.java
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.muates.inventorymanagementsystem.repository;
 import com.muates.inventorymanagementsystem.common.repository.BaseRepository;
 import com.muates.inventorymanagementsystem.model.entity.Product;
 
+import java.util.List;
+
 public interface ProductRepository extends BaseRepository<Product, Integer> {
+    List<Product> findAllBySupplierId(Integer supplierId);
 }
--- InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/repository/impl/ProductRepositoryImpl.java
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/repository/impl/ProductRepositoryImpl.java
@@ -5,10 +5,49 @@ import com.muates.inventorymanagementsystem.model.entity.Product;
 import com.muates.inventorymanagementsystem.repository.ProductRepository;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductRepositoryImpl extends BaseRepositoryImpl<Product, Integer> implements ProductRepository {
 
+    private final Connection connection;
+
     public ProductRepositoryImpl(Connection connection) {
         super(connection, Product.class);
+        this.connection = connection;
     }
+
+    @Override
+    public List<Product> findAllBySupplierId(Integer supplierId) {
+        List<Product> products = new ArrayList<>();
+        String query = "SELECT * FROM product WHERE supplier_id = ? ORDER BY id";
+
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, supplierId);
+            ResultSet resultSet = stmt.executeQuery();
+
+            while (resultSet.next()) {
+                products.add(mapProduct(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return products;
+    }
+
+    private Product mapProduct(ResultSet resultSet) throws SQLException {
+        Product product = new Product();
+        product.setId(resultSet.getInt("id"));
+        product.setName(resultSet.getString("name"));
+        product.setStockQuantity(resultSet.getInt("stock_quantity"));
+        product.setPrice(resultSet.getBigDecimal("price"));
+        product.setDiscount(resultSet.getBigDecimal("discount"));
+        product.setSupplierId(resultSet.getInt("supplier_id"));
+        return product;
+    }
+
 }
