df05074cf440b89054c8409415bcad69c2f17508
Murat Ates <mrtt.atss@gmail.com>
2025-01-03 16:57:53 +0300

    refactoring

--- InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/model/dto/bill/response/BillResponse.java
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/model/dto/bill/response/BillResponse.java
@@ -1,9 +1,11 @@
 package com.muates.inventorymanagementsystem.model.dto.bill.response;
 
+import com.muates.inventorymanagementsystem.model.dto.billdetail.response.BillDetailResponse;
 import com.muates.inventorymanagementsystem.model.enums.BillStatus;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.util.List;
 
 public class BillResponse {
 
@@ -13,6 +15,10 @@ public class BillResponse {
     private BillStatus status;
     private BigDecimal totalAmount;
     private Timestamp date;
+    private List<BillDetailResponse> billDetails;
+
+    public BillResponse() {
+    }
 
     public BillResponse(Integer id, Integer supplierId, Integer retailerId, BillStatus status, BigDecimal totalAmount, Timestamp date) {
         this.id = id;
@@ -21,6 +27,16 @@ public class BillResponse {
         this.status = status;
         this.totalAmount = totalAmount;
         this.date = date;
+    }
+
+    public BillResponse(Integer id, Integer supplierId, Integer retailerId, BillStatus status, BigDecimal totalAmount, Timestamp date, List<BillDetailResponse> billDetails) {
+        this.id = id;
+        this.supplierId = supplierId;
+        this.retailerId = retailerId;
+        this.status = status;
+        this.totalAmount = totalAmount;
+        this.date = date;
+        this.billDetails = billDetails;
     }
 
     public Integer getId() {
@@ -69,5 +85,13 @@ public class BillResponse {
 
     public void setDate(Timestamp date) {
         this.date = date;
+    }
+
+    public List<BillDetailResponse> getBillDetails() {
+        return billDetails;
+    }
+
+    public void setBillDetails(List<BillDetailResponse> billDetails) {
+        this.billDetails = billDetails;
     }
 }
