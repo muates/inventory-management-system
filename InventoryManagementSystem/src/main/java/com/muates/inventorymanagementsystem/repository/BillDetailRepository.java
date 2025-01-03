761e76647df1667f7f0ed0bca4a1a39d3601a91e
Murat Ates <mrtt.atss@gmail.com>
2025-01-03 16:58:27 +0300

    find by bill id added

--- InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/repository/BillDetailRepository.java
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/repository/BillDetailRepository.java
@@ -3,5 +3,8 @@ package com.muates.inventorymanagementsystem.repository;
 import com.muates.inventorymanagementsystem.common.repository.BaseRepository;
 import com.muates.inventorymanagementsystem.model.entity.BillDetail;
 
+import java.util.List;
+
 public interface BillDetailRepository extends BaseRepository<BillDetail, Integer> {
+    List<BillDetail> findByBillId(Integer billId);
 }
--- InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/repository/impl/BillDetailRepositoryImpl.java
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/repository/impl/BillDetailRepositoryImpl.java
@@ -5,10 +5,49 @@ import com.muates.inventorymanagementsystem.model.entity.BillDetail;
 import com.muates.inventorymanagementsystem.repository.BillDetailRepository;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BillDetailRepositoryImpl extends BaseRepositoryImpl<BillDetail, Integer> implements BillDetailRepository {
 
+    private final Connection connection;
+
     public BillDetailRepositoryImpl(Connection connection) {
         super(connection, BillDetail.class);
+        this.connection = connection;
     }
+
+    @Override
+    public List<BillDetail> findByBillId(Integer billId) {
+        List<BillDetail> billDetails = new ArrayList<>();
+        String query = "SELECT * FROM bill_detail WHERE bill_id = ? ORDER BY id";
+
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, billId);
+            ResultSet resultSet = stmt.executeQuery();
+
+            while (resultSet.next()) {
+                billDetails.add(mapBillDetail(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return billDetails;
+    }
+
+    private BillDetail mapBillDetail(ResultSet resultSet) throws SQLException {
+        BillDetail billDetail = new BillDetail();
+        billDetail.setId(resultSet.getInt("id"));
+        billDetail.setBillId(resultSet.getInt("bill_id"));
+        billDetail.setProductId(resultSet.getInt("product_id"));
+        billDetail.setQuantity(resultSet.getInt("quantity"));
+        billDetail.setUnitPrice(resultSet.getBigDecimal("unit_price"));
+        billDetail.setTotalPrice(resultSet.getBigDecimal("total_price"));
+        return billDetail;
+    }
+
 }
