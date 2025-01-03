d5a53fe2b463baa4ffbc0d2e9966e2892f1e33a8
Murat Ates <mrtt.atss@gmail.com>
2025-01-03 16:58:12 +0300

    updated

--- InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/converter/BillDetailConverter.java
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/converter/BillDetailConverter.java
@@ -1,9 +1,12 @@
 package com.muates.inventorymanagementsystem.converter;
 
+import com.muates.inventorymanagementsystem.model.dto.billdetail.response.BillDetailResponse;
 import com.muates.inventorymanagementsystem.model.dto.cart.request.CartItem;
 import com.muates.inventorymanagementsystem.model.entity.BillDetail;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BillDetailConverter {
 
@@ -19,5 +22,24 @@ public class BillDetailConverter {
                 item.getUnitPrice(),
                 totalPrice
         );
+    }
+
+    public static BillDetailResponse toDto(BillDetail entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        return new BillDetailResponse(
+                entity.getId(),
+                entity.getBillId(),
+                entity.getProductId(),
+                entity.getQuantity(),
+                entity.getUnitPrice(),
+                entity.getTotalPrice()
+        );
+    }
+
+    public static List<BillDetailResponse> toDto(List<BillDetail> entities) {
+        return entities.stream().map(BillDetailConverter::toDto).collect(Collectors.toList());
     }
 }
