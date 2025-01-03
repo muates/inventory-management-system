ec4ba652355935f081f92281cec71af390475851
Murat Ates <mrtt.atss@gmail.com>
2025-01-03 16:58:46 +0300

    find by user id added

--- InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/repository/BillRepository.java
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/repository/BillRepository.java
@@ -3,5 +3,8 @@ package com.muates.inventorymanagementsystem.repository;
 import com.muates.inventorymanagementsystem.common.repository.BaseRepository;
 import com.muates.inventorymanagementsystem.model.entity.Bill;
 
+import java.util.List;
+
 public interface BillRepository extends BaseRepository<Bill, Integer> {
+    List<Bill> findByUserId(String columnName, Integer userId);
 }
--- InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/repository/impl/BillRepositoryImpl.java
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/repository/impl/BillRepositoryImpl.java
@@ -2,13 +2,53 @@ package com.muates.inventorymanagementsystem.repository.impl;
 
 import com.muates.inventorymanagementsystem.common.repository.impl.BaseRepositoryImpl;
 import com.muates.inventorymanagementsystem.model.entity.Bill;
+import com.muates.inventorymanagementsystem.model.enums.BillStatus;
 import com.muates.inventorymanagementsystem.repository.BillRepository;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BillRepositoryImpl extends BaseRepositoryImpl<Bill, Integer> implements BillRepository {
 
+    private final Connection connection;
+
     public BillRepositoryImpl(Connection connection) {
         super(connection, Bill.class);
+        this.connection = connection;
     }
+
+    @Override
+    public List<Bill> findByUserId(String columnName, Integer userId) {
+        List<Bill> bills = new ArrayList<>();
+        String query = "SELECT * FROM bill WHERE " + columnName + " = ? ORDER BY date DESC, id DESC";
+
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, userId);
+            ResultSet resultSet = stmt.executeQuery();
+
+            while (resultSet.next()) {
+                bills.add(mapBill(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return bills;
+    }
+
+    private Bill mapBill(ResultSet resultSet) throws SQLException {
+        Bill bill = new Bill();
+        bill.setId(resultSet.getInt("id"));
+        bill.setSupplierId(resultSet.getInt("supplier_id"));
+        bill.setRetailerId(resultSet.getInt("retailer_id"));
+        bill.setStatus(BillStatus.valueOf(resultSet.getString("status").toUpperCase()));
+        bill.setTotalAmount(resultSet.getBigDecimal("total_amount"));
+        bill.setDate(resultSet.getTimestamp("date"));
+        return bill;
+    }
+
 }
