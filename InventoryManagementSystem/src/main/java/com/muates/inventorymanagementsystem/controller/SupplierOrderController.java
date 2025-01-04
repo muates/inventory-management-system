f239a081e480f4b34fc598b2f70376048565aa90
Murat Ates <mrtt.atss@gmail.com>
2025-01-04 13:26:09 +0300

    supplier order controller added

---
+++ InventoryManagementSystem/src/main/java/com/muates/inventorymanagementsystem/controller/SupplierOrderController.java
@@ -0,0 +1,40 @@
+package com.muates.inventorymanagementsystem.controller;
+
+import com.muates.inventorymanagementsystem.common.ioc.DependencyManager;
+import com.muates.inventorymanagementsystem.common.ioc.Inject;
+import com.muates.inventorymanagementsystem.model.dto.order.response.OrderResponse;
+import com.muates.inventorymanagementsystem.service.order.OrderService;
+import com.muates.inventorymanagementsystem.session.SessionManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/supplier/order")
+public class SupplierOrderController extends HttpServlet {
+
+    @Inject
+    private OrderService orderService;
+
+    @Override
+    public void init() throws ServletException {
+        DependencyManager.getContainer().injectDependencies(this);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Integer supplierId = SessionManager.getUserId(req);
+
+        boolean isSupplier = Boolean.TRUE.equals(SessionManager.isSupplier(req));
+
+        List<OrderResponse> orders = orderService.findOrders(supplierId, isSupplier);
+
+        req.setAttribute("orders", orders);
+
+        req.getRequestDispatcher("/views/supplier/order/order.jsp").forward(req, resp);
+    }
+}
